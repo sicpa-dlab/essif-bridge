dca915b013cf46642d97042651b76293d743c9b9
HossamEddine BLAACHA <houssameddine.blaacha@sicpa.com>
2021-02-24 12:39:39 +0100

    Set up colors and typography for project

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from "react-router-dom";
 
+import './index.scss'
 import App from './app/components/App';
 
 ReactDOM.render(
@@ -12,4 +13,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
