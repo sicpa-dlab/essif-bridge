0e58a6f5988551635f69ceebee6781abee2e1378
HossamEddine BLAACHA <houssameddine.blaacha@sicpa.com>
2021-02-24 15:43:53 +0100

    feat(verifier): create verifier page

--- frontend/src/app/components/App.tsx
+++ frontend/src/app/components/App.tsx
@@ -1,12 +1,13 @@
 import { Route, Switch } from "react-router-dom";
 
-import { Home, Issuer } from '../pages';
+import { Home, Issuer, Verifier } from '../pages';
 
 const App: React.FC = () => {
   return (
     <Switch>
       <Route path="/" exact component={Home}></Route>
       <Route path="/issuer" component={Issuer}></Route>
+      <Route path="/verifier" component={Verifier}></Route>
     </Switch>
   );
 }
