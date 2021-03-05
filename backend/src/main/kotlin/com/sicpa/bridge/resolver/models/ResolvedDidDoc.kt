9ea450eec506d4d842585049f53ea96be62fb663
Matteo Marangoni <matteo.marangoni@sicpa.com>
2021-03-05 12:45:40 +0100

    DidDoc model changed in universal resolver

--- backend/src/main/kotlin/com/sicpa/bridge/resolver/models/ResolvedDidDoc.kt
+++ backend/src/main/kotlin/com/sicpa/bridge/resolver/models/ResolvedDidDoc.kt
@@ -2,6 +2,6 @@ package com.sicpa.bridge.resolver.models
 
 data class ResolvedDidDoc(
     val publicKey: List<PublicKey>? = null,
-    val assertionMethod: List<String>? = null,
+    //val assertionMethod: List<Any>? = null,
     val verificationMethod: List<VerificationMethod>? = null
 )
