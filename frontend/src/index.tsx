f1f0acb898e9594d97135d932ca7a5a9b93da7b7
HossamEddine BLAACHA <houssameddine.blaacha@sicpa.com>
2021-03-11 15:59:56 +0100

    add footer to the app

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,11 +4,15 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 import './index.scss'
 import App from './app/components/App';
+import Footer from './app/components/footer';
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <>
+        <App />
+        <Footer />
+      </>
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
