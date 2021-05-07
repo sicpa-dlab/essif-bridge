f515cc3c536cd30692bbb12b3bcefc81eb16d79e
Matteo Marangoni <matteo.marangoni@sicpa.com>
2021-05-07 16:12:39 +0200

    crossorigin endpoint

--- backend/src/main/kotlin/com/sicpa/bridge/api/annoncreds/Verifications.kt
+++ backend/src/main/kotlin/com/sicpa/bridge/api/annoncreds/Verifications.kt
@@ -2,14 +2,24 @@ package com.sicpa.bridge.api.annoncreds
 
 import com.sicpa.bridge.api.annoncreds.domain.VerificationTemplateUseCase
 import com.sicpa.bridge.api.annoncreds.domain.VerificationUseCase
-import com.sicpa.bridge.api.annoncreds.domain.model.*
+import com.sicpa.bridge.api.annoncreds.domain.model.Verification
+import com.sicpa.bridge.api.annoncreds.domain.model.VerificationCreate
+import com.sicpa.bridge.api.annoncreds.domain.model.VerificationTemplate
+import com.sicpa.bridge.api.annoncreds.domain.model.VerificationTemplateCreate
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
-import org.springframework.web.bind.annotation.*
 import javax.validation.Valid
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @Tag(name = "Verifications", description = "The Verifications API")
+@CrossOrigin
+
 class Verifications (
     private val verificationUseCase: VerificationUseCase,
     private val verificationTemplateUseCase : VerificationTemplateUseCase
